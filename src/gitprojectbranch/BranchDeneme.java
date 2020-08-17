fee2b7cf45579322e0d41f08b83c1ef8104d2aa4
CelilCirak <celilcirak@gmail.com>
2020-08-16 22:57:47 -0400

    featureden commit

--- src/gitprojectbranch/BranchDeneme.java
+++ src/gitprojectbranch/BranchDeneme.java
@@ -5,6 +5,8 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 		System.out.println("12:15 master branch'den ilk komut");
 		
+		System.out.println("12:35 feature branch'den ilk komut");
+		
 	}
 
 }
